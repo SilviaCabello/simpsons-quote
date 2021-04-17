fc212add9df9d4633d28061641ee6849d9292249
SilviaCabello <silviacabellom@gmail.com>
2021-04-17 16:19:59 +0200

    add infto to QuoteCard

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,12 +1,13 @@
-function QuoteCard ( {quote} ) {
-    return (
-        quote &&
-            <div className="QuoteCard">
-                <img src={quote.image} alt={quote.character} />
-            </div>
+function QuoteCard({ quote }) {
+  return (
+    quote && (
+      <div className="QuoteCard">
+        <img src={quote.image} alt={quote.character} />
+        <h3>Name: {quote.character}</h3>
+        <p>Quote: {quote.quote}</p>
+      </div>
     )
+  );
 }
 
-
 export default QuoteCard;
-
